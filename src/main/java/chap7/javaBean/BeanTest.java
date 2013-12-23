15a1259df1c580465667108548434dd752c59ef1
kenshin579 <kenshin579@hotmail.com>
2013-12-23 15:10:45 +0900

    chap7 소스 추가

---
+++ src/main/java/chap7/javaBean/BeanTest.java
@@ -0,0 +1,13 @@
+package chap7.javaBean;
+
+public class BeanTest {
+    private String name = "honggildong";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
